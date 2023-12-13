bc83bbd308ffed87df8fea87bccc95e5f1a6e4d2
Ahmed Abdelsamad <a.m.abdelsamed@gmail.com>
2023-12-13 18:14:37 +0200

    Add files via upload

---
+++ Azure/AKS-V1/0-locals.tf
@@ -0,0 +1,7 @@
+locals {
+  env                 = "prod"
+  region              = "UK South"
+  resource_group_name = "prodRG"
+  eks_name            = "eksCluster"
+  eks_version         = "1.27"
+}
